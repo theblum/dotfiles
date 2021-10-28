c0929e794094386b46fcf9f530fc2c2d732db6d9
Brian Blumberg <blum@disroot.org>
2021-10-28 13:45:43 -0700

    Add lua lsp and completion

--- nvim/lua/blum/cfg_lsp.lua
+++ nvim/lua/blum/cfg_lsp.lua
@@ -4,3 +4,29 @@ local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protoco
 lspconfig.clangd.setup{ capabilities = capabilities }
 lspconfig.rust_analyzer.setup{ capabilities = capabilities, cmd = { "rustup", "run", "nightly", "rust-analyzer" } }
 lspconfig.zls.setup{ capabilities = capabilities }
+lspconfig.vimls.setup{ capabilities = capabilities }
+
+local sumneko_root_path = vim.fn.stdpath('cache')..'/lspconfig/sumneko_lua/lua-language-server'
+local lua_runtime_path = vim.split(package.path, ';')
+table.insert(lua_runtime_path, "lua/?.lua")
+table.insert(lua_runtime_path, "lua/?/init.lua")
+
+lspconfig.sumneko_lua.setup{
+    cmd = { "lua-language-server", "-E", sumneko_root_path .. "/main.lua" },
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+                path = lua_runtime_path,
+            },
+
+            diagnostics = {
+                globals = { "vim" },
+            },
+
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+        }
+    },
+}
