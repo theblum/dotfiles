44ffd3014ace8d52a822a3d95884c90f60f69bd9
Brian Blumberg <blum@disroot.org>
2021-10-29 20:40:24 -0700

    Back to base16 colorschemes again...

--- nvim/lua/blum/colors.lua
+++ nvim/lua/blum/colors.lua
@@ -1,28 +1,33 @@
 require("lualine").setup({
     options = {
-        -- NOTE: This is the lualine material theme, not the material-nvim one that comes with
-        -- `marko-cerovac/material.nvim`
+        -- theme = "material-nvim",
         theme = "material",
     }
 })
 
-require("material").setup({
-    contrast = true,
-    borders = true,
-    popup_menu = "dark",
+local vimrc_background = vim.fn.expand("$HOME/.vimrc_background")
+if vim.fn.filereadable(vimrc_background) then
+    vim.g.base16colorspace = 256
+    vim.cmd("source " .. vimrc_background)
+end
 
-    italics = {
-        comments = true,
-    },
-
-    contrast_windows = {
-        "qf"
-    },
-
-    disable = {
-        eob_lines = true,
-    }
-})
-
-vim.g.material_style = "oceanic"
-vim.cmd('colorscheme material')
+-- require("material").setup({
+--     contrast = false,
+--     borders = true,
+--     popup_menu = "dark",
+--
+--     italics = {
+--         comments = true,
+--     },
+--
+--     contrast_windows = {
+--         "qf"
+--     },
+--
+--     disable = {
+--         eob_lines = true,
+--     }
+-- })
+--
+-- vim.g.material_style = "oceanic"
+-- vim.cmd('colorscheme material')
