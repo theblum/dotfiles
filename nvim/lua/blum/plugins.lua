885f48d64cd390ca2431a06a2a012b7434347312
Brian Blumberg <blum@disroot.org>
2022-02-22 16:54:57 -0800

    Add snippet manager to nvim

--- nvim/lua/blum/plugins.lua
+++ nvim/lua/blum/plugins.lua
@@ -22,6 +22,8 @@ return require("packer").startup(function(use)
     use "hrsh7th/cmp-path"
     use "hrsh7th/cmp-buffer"
     use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-vsnip"
+    use "hrsh7th/vim-vsnip"
     use { "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } }
     use "nvim-telescope/telescope-fzy-native.nvim"
     use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
